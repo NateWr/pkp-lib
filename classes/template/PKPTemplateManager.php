7473918a9f11a433689cfe091d9d65acf654601e
Nate Wright <natew@notthisway.com>
2026-05-06 10:46:14 +0100

    Add: support recursion in navigation menu templates

--- classes/template/PKPTemplateManager.php
+++ classes/template/PKPTemplateManager.php
@@ -1883,7 +1883,7 @@ class PKPTemplateManager extends Smarty
 
     /**
      * Smarty modifier: json_encode_html_attribute
-     * 
+     *
      * Encodes a value to JSON with full HTML-attribute safety.
      * Escapes ", ', <, >, & as \u0022, \u0027, \u003C, \u003E, \u0026
      * so the output can be safely placed inside any HTML attribute
@@ -2631,6 +2631,7 @@ class PKPTemplateManager extends Smarty
             'id' => $params['id'],
             'ulClass' => $params['ulClass'] ?? '',
             'liClass' => $params['liClass'] ?? '',
+            'items' => $navigationMenu?->menuTree ?? [],
         ]);
 
         return $this->fetch($menuTemplatePath);
